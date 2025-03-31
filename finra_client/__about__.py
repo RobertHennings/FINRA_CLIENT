9766b89645cea5b8010da67662670f76b4fc511f
Robert Hennings <hennings.robert@web.de>
2025-03-31 18:33:28 +0200

    DOC: Added deployment files for PyPi

---
+++ finra_client/__about__.py
@@ -0,0 +1,7 @@
+__title__ = "finra_client"
+__about__ = "The finra_client enables users to interact and foremost download data from the Finra (Rest) API and acts as a comfortable Wrapper Client for Python."
+__version__ = '0.0.1'
+__author__ = 'Robert Hennings'
+__email__ = "hennings.robert[at]web.de"
+__url__ = "https://github.com/RobertHennings/Finra_CLIENT"
+__keywords__ = "Finra, Finra-API, FINRA API, Finra Python API"
